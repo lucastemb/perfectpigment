cbc15648349180838098b339a53d8a62eb8c7744
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2023-06-17 00:56:04 -0400

    Updated Version (2022.3.2f1)
    Includes shooting mechanism

---
+++ My project (2)/Assets/Bullet.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public GameObject enemy; 
+    void Awake(){
+        Destroy(gameObject, 3);
+    }
+
+}
