7b9a65ea6c56a1b27e499e6bc85693ea5cb33df9
josecast1 <jc88377@gmail.com>
2023-06-17 23:33:57 -0400

    Timer, Gravity, Jump

---
+++ My project (2)/Assets/Stopwatch.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Stopwatch : MonoBehaviour
+{
+    bool stopwatchActive = true;
+    float currentTime;
+    public Text currentTimeText;
+    public Text finishText;
+
+    void Start()
+    {
+        currentTime = 0;
+    }
+    
+    void Update()
+    {
+        
+        if (stopwatchActive == true) {
+            currentTime = currentTime + Time.deltaTime;
+        }    
+        TimeSpan time = TimeSpan.FromSeconds(currentTime);
+        currentTimeText.text = time.ToString(@"mm\:ss\:fff");
+        
+    }
+
+    public void StartStopwatch() {
+        stopwatchActive = true;
+    }
+
+    public void StopStopwatch() {
+        stopwatchActive = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        finishText.text = "Final Time: " + currentTimeText.text;
+        stopwatchActive = false;
+    }
+}
