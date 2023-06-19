92147a93d6b7d172a3b2796ad3971e3802f26351
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2023-06-19 16:43:23 -0400

    Main Game
    Fixed trampoline physics

--- My project (2)/Assets/Scripts/Trampoline.cs
+++ My project (2)/Assets/Scripts/Trampoline.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Trampoline : MonoBehaviour
 {
-    void OnCollisionEnter(Collision col){
+    void OnTriggerEnter(Collider col){
         if(col.gameObject.CompareTag("Player")){
+            Debug.Log("hit");
             PlayerMovement2.velocity.y = Mathf.Sqrt(14f * -2f * PlayerMovement2.gravity);
         }
     }
