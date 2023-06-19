7cb86c248e1a6d066b7e68314e7bcbb6fbdd673f
cgbravos <134538670+cgbravos@users.noreply.github.com>
2023-06-19 17:48:55 -0400

    Pause Menu

---
+++ Planet Paper/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+        
+    public static bool gamePaused = false; 
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gamePaused)
+            {
+                Resume();
+            }
+            else 
+            {
+                Pause();
+            }
+
+        }
+    }
+
+    public void Resume ()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+
+    void Pause ()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gamePaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Loading Menu...");
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quiting Game...");
+        Application.Quit();
+    }
+}
